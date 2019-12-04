000cb1bf3cc5d3c78deabb6a16aeb02b57dc7ae2
charcalope <47309188+charcalope@users.noreply.github.com>
2019-12-04 11:49:08 -0800

    Update make_corpus.py

--- make_corpus.py
+++ make_corpus.py
@@ -1,5 +1,36 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import KMeans
+from TreeStuff import TaskTree
+from Solution import Solution
+
+
+def label_trees(tree_list, labels):
+    def func(ordering):   return list(map(lambda x: labels[x], ordering))
+
+    new_tree_list = []
+    for preo, ino in tree_list:
+        new_tree_list.append((func(preo), func(ino)))
+
+    return new_tree_list
+
+
+def file2nodes(filename):
+    nodes = []
+    with open(filename, 'rt') as f:
+        for line in f:
+            nodes.append(line)
+    return nodes
+
+
+def file2trees(filename):
+    trees = []
+    with open(filename, 'rt') as f:
+        while f:
+            preo = next(f).strip('pre: ').split(',')
+            ino = next(f).strip('in: ').split(',')
+            trees.append((preo, ino))
+    return trees
+
 
 def kmeans(corpus) -> dict:
     vectorizer = TfidfVectorizer(max_features=4)
@@ -20,3 +51,11 @@ def kmeans(corpus) -> dict:
         label_dict[doc] = label
 
     return label_dict
+
+
+def make_corpus(treefile, nodefile):
+    nodes = file2nodes(nodefile)
+    node_dict = kmeans(nodes)
+
+    tree_list = file2trees(treefile)
+    labeled_trees = label_trees(tree_list, node_dict)
